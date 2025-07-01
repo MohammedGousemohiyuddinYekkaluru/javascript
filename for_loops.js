228c129d6ac4074d52da4a2e9cc0e26f19a08910
mohammed gouse <gousemohammed163@gmail.com>
2025-07-01 13:10:04 +0530

    Added for loops

--- for_loops.js
+++ for_loops.js
@@ -1,8 +1,17 @@
 //for loop
+// Adding first n natural numbers
+// let sum = 0;
+// let n = Number(prompt("Enter the value of n"));
+// for(let i = 0; i < n; i++){
+//     sum += (i+1);
+// }
+// console.log(sum);
 
-let sum = 0;
-let n = Number(prompt("Enter the value of n"));
-for(let i = 0; i < n; i++){
-    sum += (i+1);
+//factorial by using for loop
+
+let fact = 1;
+let num = 5;
+for(let i = 1; i <= num; i++){
+    fact *= i;
 }
-console.log(sum);
+console.log(fact);
