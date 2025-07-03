e974675e9e68d7ea4b21a74ae58ba3f8fbd14664
mohammed gouse <gousemohammed163@gmail.com>
2025-07-03 11:40:22 +0530

    Add Functions

---
+++ functions.js
@@ -0,0 +1,23 @@
+const hello = () => {
+    console.log("Hey how are you");
+    
+}
+
+function onePlusAvg(x,y){
+    console.log("Done"); 
+    return 1 + (x + y) / 2;
+};
+
+const sum = (p,q) => {
+    return p + q;
+};
+let a = 1;
+let b = 2;
+let c = 3;
+
+hello();
+console.log(onePlusAvg(a,b));
+console.log(onePlusAvg(b,c));
+console.log(onePlusAvg(a,c));
+console.log(sum(9,8));
+
