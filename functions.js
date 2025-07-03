46e90a33c711517851d691109e53de9a0e0390e7
mohammed gouse <gousemohammed163@gmail.com>
2025-07-03 12:03:03 +0530

    Add Functions

--- functions.js
+++ functions.js
@@ -20,3 +20,19 @@ console.log(onePlusAvg(b,c));
 console.log(onePlusAvg(a,c));
 console.log(sum(9,8));
 
+//Add two numbers
+let addTwoNumbers = (num1, num2) => {
+    console.log(num1 + num2);
+}
+
+let result = addTwoNumbers(3, 7);
+console.log(result); //undefined due to no value is returned from the function.
+
+let otherExample = (num3, num4) => {
+    let result = num3 + num4;
+    let over = "done";
+    return {result, over};
+}
+
+let output = otherExample(5,6);
+console.log(output);
