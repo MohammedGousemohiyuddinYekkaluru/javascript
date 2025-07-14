007c8fbd406d53e785bf7c5ff4c849be1b140d99
mohammed gouse <gousemohammed163@gmail.com>
2025-07-14 11:42:52 +0530

    Add Template literals

--- strings.js
+++ strings.js
@@ -2,3 +2,8 @@ let fName = "gouse"; // can create strings using single and double quotes.
 console.log(fName.length);
 console.log(fName[0],fName[1]); //Strings indices starts from 0.
 
+//Template literals
+
+let boy1 = "Gouse";
+let boy2 = "jammu";
+console.log(`${boy1} is a freind of ${boy2}.`); //this is the use of template literals
