442afaa02b9ad46e331753ef3e1a31c527f248e3
mohammed gouse <gousemohammed163@gmail.com>
2025-07-14 11:36:02 +0530

    Update string

--- strings.js
+++ strings.js
@@ -1,2 +1,4 @@
-let name = "gouse"; // can create strings using single and double quotes.
-console.log(name.length);
+let fName = "gouse"; // can create strings using single and double quotes.
+console.log(fName.length);
+console.log(fName[0],fName[1]); //Strings indices starts from 0.
+
