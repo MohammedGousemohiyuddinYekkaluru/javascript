0db53b984cac7206263c9a50f1266e0e72958279
mohammed gouse <gousemohammed163@gmail.com>
2025-07-14 11:50:05 +0530

    Strings

--- strings.js
+++ strings.js
@@ -6,4 +6,11 @@ console.log(fName[0],fName[1]); //Strings indices starts from 0.
 
 let boy1 = "Gouse";
 let boy2 = "jammu";
-console.log(`${boy1} is a freind of ${boy2}.`); //this is the use of template literals
+console.log(`${boy1} is a freind of ${boy2}.`); //this is the use of template literals. it is also called string interpolation.
+
+//Escape sequence
+
+let fruit1 = "Bana\'na";
+console.log(fruit1);
+let fruit2 = "man\tgo"; //tab space
+console.log(fruit2);
