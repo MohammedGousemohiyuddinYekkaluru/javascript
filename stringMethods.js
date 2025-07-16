c689c37525aad71918edc725b6c008cd04af37c9
mohammed gouse <gousemohammed163@gmail.com>
2025-07-16 11:17:55 +0530

    StringMethods

--- stringMethods.js
+++ stringMethods.js
@@ -1,4 +1,9 @@
 let naam = "Gouse";
+let naam2 = "Mohammed";
 console.log(naam.lenght); //5 //property
 console.log(naam.toUpperCase()); //method 
 console.log(naam.toUpperCase()); //method 
+console.log(naam.slice(2,4)); //us //4 is not included 
+console.log(naam.slice(2)); //use 
+console.log(naam.replace("s","$"));//Gou$e
+console.log(naam.concat(naam2)); //Gouse Mohammed
