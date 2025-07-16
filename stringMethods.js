24bd8538c531e88cb99b07f945b8667030d6befc
mohammed gouse <gousemohammed163@gmail.com>
2025-07-16 11:26:03 +0530

    StringMethods

--- stringMethods.js
+++ stringMethods.js
@@ -1,5 +1,6 @@
 let naam = "Gouse";
 let naam2 = "Mohammed";
+let naam3 = "   Mohiyuddin    "
 console.log(naam.lenght); //5 //property
 console.log(naam.toUpperCase()); //method 
 console.log(naam.toUpperCase()); //method 
@@ -7,3 +8,7 @@ console.log(naam.slice(2,4)); //us //4 is not included
 console.log(naam.slice(2)); //use 
 console.log(naam.replace("s","$"));//Gou$e
 console.log(naam.concat(naam2)); //Gouse Mohammed
+console.log(naam3.trim()); //"Mohiyuudin"
+
+//strings are immutable
+console.log(naam, naam2, naam3);
