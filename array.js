c82dec3bf23d4b7ad3bcb1450ed1a668511e4312
mohammed gouse <gousemohammed163@gmail.com>
2025-07-21 10:08:44 +0530

    Arrays

---
+++ array.js
@@ -0,0 +1,10 @@
+let class_marks = [91, 92, 61, 62, false, null];//collection of items,can hold more than one value and one datatype.
+console.log(class_marks);
+console.log(class_marks[0]);
+console.log(class_marks[1]);
+class_marks[6] = 88; //adding a new value //arrays are mutable.
+class_marks[1] = 78; //changing the value of 1st index.
+console.log(class_marks); 
+console.log(`The length of class_marks is ${class_marks.length}`);
+console.log(typeof(class_marks)); //object.
+
