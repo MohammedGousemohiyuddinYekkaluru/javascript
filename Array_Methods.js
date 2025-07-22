09cbcbef4c5e96c6fe7fdb5ef71c0c229bbd4b1a
mohammed gouse <gousemohammed163@gmail.com>
2025-07-22 12:26:17 +0530

    Array Methods

---
+++ Array_Methods.js
@@ -0,0 +1,20 @@
+ let arr = [1,2,3,4];
+ let b = arr.toString(); //b is now a string
+ console.log(b);
+ let c = arr.join(" and ") 
+ console.log(c,typeof c); //string
+//  let d = arr.pop(); //deletes last value in the array.
+//  console.log(d); //4 :- gives the deleted value.
+//  console.log(arr);
+//  let e = arr.push(5); //push returns new array length
+//  console.log(arr,arr.length);
+
+// let f = arr.shift();//removes first value in the array.
+// console.log(arr);
+let g = arr.unshift(0); //adds new value to the beggining of the array.
+console.log(arr);
+
+ 
+ 
+
+ 
