9cde029d141238e5119ab6effbefedf552f64846
mohammed gouse <gousemohammed163@gmail.com>
2025-07-23 09:57:28 +0530

    Array Methods

--- Array_Methods.js
+++ Array_Methods.js
@@ -14,6 +14,42 @@
 let g = arr.unshift(0); //adds new value to the beggining of the array.
 console.log(arr);
 
+//array methods 2
+let num = [1,2,3,4,5,6,7,8,9];
+// delete num[0]; //operator
+// console.log(num,num.length); //10 //doesn't change the length.
+let num2 = [10,11,12];
+let num3 =[13,14,15];
+// let newarr = num.concat(num2,num3);
+// console.log(newarr);
+
+//sort method
+
+let compare = (a,b) => {
+    return a - b //ascending order
+    b-a //descending order
+}
+let sortNum = [22,6,33,99,65];
+sortNum.sort(compare) //changes original array
+console.log(sortNum);
+
+//splice and slice
+
+let spliceArr = [22,6,33,99,65];
+let deletedValues = spliceArr.splice(1,1,55,65);
+console.log(spliceArr); //changes original array
+console.log(deletedValues); //returns deleted values.
+
+
+let sliceArr = [22,6,33,99,65];
+let newsliceArr = sliceArr.slice(0,4); //returns new array.
+console.log(newsliceArr);
+
+
+
+
+
+
  
  
 
